5900553164ecc854dd13d05429923feabc6ba6c9
dpcl2397 <dpcl2397@gmail.com>
2024-05-19 18:19:10 +0530

    signupdata json added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 
@@ -11,38 +12,38 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'newsPortal';
   searchText: any;
-  constructor(private router: Router, private formBuilder: FormBuilder, private fb: FormBuilder) {
+  constructor(private router: Router, private formBuilder: FormBuilder, private fb: FormBuilder, private http: HttpClient) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]]
     });
   }
-  navigate(data: any) {
-    localStorage.setItem('title', data);
-    this.router.navigate(['/category', data.toLowerCase().replace(/\s+/g, '-')]);
+  // navigate(data: any) {
+  //   localStorage.setItem('title', data);
+  //   this.router.navigate(['/category', data.toLowerCase().replace(/\s+/g, '-')]);
 
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-  }
-  aboutus() {
-    this.router.navigate(['about'])
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-  }
-  contactus() {
-    this.router.navigate(['contact'])
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-  }
+  //   window.scroll({
+  //     top: 0,
+  //     left: 0,
+  //     behavior: 'smooth'
+  //   });
+  // }
+  // aboutus() {
+  //   this.router.navigate(['about'])
+  //   window.scroll({
+  //     top: 0,
+  //     left: 0,
+  //     behavior: 'smooth'
+  //   });
+  // }
+  // contactus() {
+  //   this.router.navigate(['contact'])
+  //   window.scroll({
+  //     top: 0,
+  //     left: 0,
+  //     behavior: 'smooth'
+  //   });
+  // }
   loginform: boolean = true
   userlogin: boolean = true;
   adminlogin: boolean = false
@@ -51,10 +52,12 @@ export class AppComponent {
   userregistration: boolean = false
   loginForm: FormGroup;
   userloginForm!: FormGroup;
+  userregistrationform!: FormGroup
   hasError(controlName: string, errorName: string) {
     const control = this.loginForm.get(controlName);
     const usercontrol = this.userloginForm.get(controlName)
-    return control?.hasError(errorName) && control?.touched, usercontrol?.hasError(errorName) && usercontrol?.touched;
+    const signcontrol = this.userregistrationform.get(controlName)
+    return control?.hasError(errorName) && control?.touched, usercontrol?.hasError(errorName) && usercontrol?.touched,signcontrol?.hasError(errorName) && signcontrol?.touched;
   }
   email: string = "dv2397@gmail.com";
   password: string = "Dv@239764"
@@ -74,27 +77,53 @@ export class AppComponent {
       useremail: ['', [Validators.required, Validators.email]],
       userpassword: ['', Validators.required]
     });
+    this.userregistrationform = new FormGroup({
+      "regname": new FormControl(),
+      "regemail": new FormControl(),
+      "regpassword": new FormControl(),
+    })
   }
-
-  // hasError(controlName: string, errorName: string): boolean {
-  //   return this.userloginForm.get(controlName).hasError(errorName);
-  // }
   useremail: string = "user2397@gmail.com"
   userpass: string = "User@239764"
-  onSubmit(useremail: string, userpassword: string) {
-    if (this.userloginForm.valid) {
-      if (this.useremail == useremail && this.userpass == userpassword) {
+  onSubmit(usermail: string, userpassword: string) {
+    // this.http.get<any>("http://localhost:3000/signupData")
+    //   .subscribe(res => {
+    //     if (res.regemail == usermail && res.regpassword == userpassword) {
+    //       this.loginasuser = true;
+    //       this.loginform = false
+    //     }
+    //     else {
+    //       alert("Password & Email does not match")
+    //     }
+
+    //   })
+    this.http.get<any[]>("http://localhost:3000/signupData").subscribe(users => {
+      const foundUser = users.find(user => user.regemail === usermail && user.regpassword === userpassword);
+      if (foundUser) {
         this.loginasuser = true;
-        this.loginform = false
+        this.loginform = false;
+        // alert(foundUser.)
+      } else {
+        alert("Password & Email do not match");
       }
-      else {
-        alert("Password & Email does not match")
-      }
-    }
+    });
   }
 
   showregistrationForm() {
     this.userregistration = true
     this.loginform = false
+  }
+  signupuser: any
+  onSubmitregister(userregistrationform: FormGroup) {
+    this.http.post("http://localhost:3000/signupData", this.userregistrationform.value)
+      .subscribe(res => {
+        alert("Sign Up Succefully");
+        this.userregistrationform.reset();
+        this.userregistration = false
+        this.loginform = true;
+      }, err => {
+        alert("Somethig Went Wrong")
+      }
+      )
   }
 }
